1a02dc3880258b7a96f6269a5611205f25bfbbe9
ajmkiii <alexander.dunne@outlook.com>
2023-05-25 23:56:46 +0100

    Add download functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, jsonify
-from scraper import scrape
+from flask import Flask, request, render_template, jsonify, send_file
+from scraper import scrape, save_to_csv
 
 app = Flask(__name__)
 
@@ -11,7 +11,12 @@ def home():
 def do_scrape():
     urls = request.json.get('urls', [])
     data = [scrape(url) for url in urls]
+    save_to_csv(data)
     return jsonify(data)
+
+@app.route('/download')
+def download():
+    return send_file('scraped_data.csv', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -6,9 +6,9 @@ def scrape(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-    
+
     response = requests.get(url, headers=headers)
-    content = response.content  
+    content = response.content
 
     # parse the HTML content with BeautifulSoup
     soup = BeautifulSoup(content, 'html.parser')
@@ -25,5 +25,15 @@ def scrape(url):
         meta_tags = soup.find_all('meta', attrs={'property': 'og:updated_time'})
         values = [tag['content'] for tag in meta_tags]
         metadata['citation_publication_date'] = ', '.join(values)
-        
+
     return metadata
+
+def save_to_csv(data):
+    fieldnames = ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date']
+
+    with open('scraped_data.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for item in data:
+            writer.writerow(item)
