cc885f7482a5de59e949a8f412acfa9c998f179e
ajmkiii <alexander.dunne@outlook.com>
2023-06-08 07:48:45 +0100

    update for embedding

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    if request.args.get('embed'):
+        return redirect(url_for('embed'))
     return render_template('index.html')
+
 
 @app.route('/scrape', methods=['POST'])
 def do_scrape():
@@ -20,3 +23,8 @@ def download():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/embed')
+def embed():
+    return render_template('embed.html')
