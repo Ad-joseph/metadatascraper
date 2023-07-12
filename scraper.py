6ec9a8029cfffc6ac7c2158ebb7a1c786fabcc0e
ad-joseph <aj.dunne@outlook.com>
2023-07-12 17:26:43 +0100

    Updated scraper.py to handle multiple tags

--- scraper.py
+++ scraper.py
@@ -2,24 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-def scrape_journals(url):
+def get_journal_urls(starting_url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(starting_url, headers=headers)
     content = response.content
 
     soup = BeautifulSoup(content, 'html.parser')
 
     journal_urls = []
-    article_links = soup.select('a[data-track-action="view full text"]')
-    for link in article_links:
-        href = link['href']
-        journal_urls.append(href)
+
+    # Find the <a> tags with class="journal-link"
+    journal_elements = soup.find_all('a', class_='journal-link')
+
+    for element in journal_elements:
+        journal_url = element['href']
+        journal_urls.append(journal_url)
 
     return journal_urls
 
-def scrape_journal_data(url):
+def scrape(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
@@ -31,7 +34,7 @@ def scrape_journal_data(url):
     metadata = {}
 
     tag_mappings = {
-        'citation_title': ['og:citation_title', 'title'],
+        'citation_title': ['og:citation_title', 'og:title', 'title'],
         'citation_author': ['og:citation_author'],
         'citation_journal_title': ['og:citation_journal_title'],
         'citation_volume': ['og:citation_volume'],
@@ -51,20 +54,19 @@ def scrape_journal_data(url):
     return metadata
 
 def save_to_csv(data):
-    fieldnames = ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date']
+    fieldnames = ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date', 'date-sold']
+
     with open('scraped_data.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+
         for item in data:
             writer.writerow(item)
 
-# Example usage:
-starting_url = url # Replace with your starting URL
-journal_urls = scrape_journals(starting_url)
-
-scraped_data = []
-for journal_url in journal_urls:
-    data = scrape_journal_data(journal_url)
-    scraped_data.append(data)
-
-save_to_csv(scraped_data)
+def scrape_all(urls):
+    scraped_data = []
+    for url in urls:
+        metadata = scrape(url)
+        scraped_data.append(metadata)
+    save_to_csv(scraped_data)
+    return scraped_data
