38a5df3bff2c4262a466416be137d56ff6cef6cb
ad-joseph <aj.dunne@outlook.com>
2023-07-12 16:36:20 +0100

    Updated scraper.py to handle multiple tags

--- scraper.py
+++ scraper.py
@@ -6,45 +6,31 @@ def scrape(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-    
     response = requests.get(url, headers=headers)
     content = response.content
-    
-    # parse the HTML content with BeautifulSoup
+
     soup = BeautifulSoup(content, 'html.parser')
-    
-    # Collect metadata
+
     metadata = {}
-    meta_tags = soup.find_all('meta')
-    
-    meta_mapping = {
-        'citation_title': ['citation_title', 'title'],
-        'citation_author': ['citation_author', 'author'],
-        'citation_journal_title': ['citation_journal_title', 'journal'],
-        'citation_volume': ['citation_volume', 'volume'],
-        'citation_issue': ['citation_issue', 'issue'],
-        'citation_publication_date': ['citation_publication_date', 'publication_date'],
-        'date-sold': ['date-sold']
+
+    tag_mappings = {
+        'citation_title': ['og:citation_title', 'title'],
+        'citation_author': ['og:citation_author'],
+        'citation_journal_title': ['og:citation_journal_title'],
+        'citation_volume': ['og:citation_volume'],
+        'citation_issue': ['og:citation_issue'],
+        'citation_publication_date': ['og:citation_publication_date']
     }
-    
-    for key, tag_names in meta_mapping.items():
-        values = []
-        for tag in meta_tags:
-            if 'name' in tag.attrs and tag.attrs['name'] in tag_names:
-                values.append(tag.attrs['content'])
-            elif 'property' in tag.attrs and tag.attrs['property'] in tag_names:
-                values.append(tag.attrs['content'])
-        
-        if key == 'citation_author':
-            # Format author names with a comma between last name and first name
-            formatted_authors = []
-            for value in values:
-                authors = value.split(', ')
-                formatted_authors.append(' '.join(authors[::-1]))
-            metadata[key] = ', '.join(formatted_authors)
-        else:
-            metadata[key] = ', '.join(values)
-    
+
+    for key, tag_names in tag_mappings.items():
+        value = ''
+        for tag_name in tag_names:
+            tag = soup.find('meta', attrs={'property': tag_name}) or soup.find('meta', attrs={'name': tag_name})
+            if tag:
+                value = tag['content']
+                break
+        metadata[key] = value
+
     return metadata
 
 def save_to_csv(data):
