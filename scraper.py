0a1418e17a4fa7c5b0333610ddf2ce27e7762261
ad-joseph <aj.dunne@outlook.com>
2023-07-12 16:02:44 +0100

    Updated scraper.py to handle multiple tags

--- scraper.py
+++ scraper.py
@@ -6,19 +6,35 @@ def scrape(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-
+    
     response = requests.get(url, headers=headers)
     content = response.content
-
+    
     # parse the HTML content with BeautifulSoup
     soup = BeautifulSoup(content, 'html.parser')
-
+    
     # Collect metadata
     metadata = {}
-
-    for key in ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date']:
-        meta_tags = soup.find_all('meta', attrs={'name': key})
-        values = [tag['content'] for tag in meta_tags]
+    meta_tags = soup.find_all('meta')
+    
+    meta_mapping = {
+        'citation_title': ['citation_title', 'title'],
+        'citation_author': ['citation_author', 'author'],
+        'citation_journal_title': ['citation_journal_title', 'journal'],
+        'citation_volume': ['citation_volume', 'volume'],
+        'citation_issue': ['citation_issue', 'issue'],
+        'citation_publication_date': ['citation_publication_date', 'publication_date'],
+        'date-sold': ['date-sold']
+    }
+    
+    for key, tag_names in meta_mapping.items():
+        values = []
+        for tag in meta_tags:
+            if 'name' in tag.attrs and tag.attrs['name'] in tag_names:
+                values.append(tag.attrs['content'])
+            elif 'property' in tag.attrs and tag.attrs['property'] in tag_names:
+                values.append(tag.attrs['content'])
+        
         if key == 'citation_author':
             # Format author names with a comma between last name and first name
             formatted_authors = []
@@ -28,20 +44,15 @@ def scrape(url):
             metadata[key] = ', '.join(formatted_authors)
         else:
             metadata[key] = ', '.join(values)
-
-    if len(metadata['citation_publication_date']) == 0:
-        meta_tags = soup.find_all('meta', attrs={'property': 'og:updated_time'})
-        values = [tag['content'] for tag in meta_tags]
-        metadata['citation_publication_date'] = ', '.join(values)
-
+    
     return metadata
 
 def save_to_csv(data):
-    fieldnames = ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date']
-
+    fieldnames = ['citation_title', 'citation_author', 'citation_journal_title', 'citation_volume', 'citation_issue', 'citation_publication_date', 'date-sold']
+    
     with open('scraped_data.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-
+        
         for item in data:
             writer.writerow(item)
